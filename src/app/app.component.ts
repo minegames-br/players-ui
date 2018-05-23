ee1120174975828ad7dda0eeca63d996c871ac5f
Joao Emilio <joaoemilio@gmail.com>
2018-05-22 22:55:51 -0300

    api security

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent  {
   accessToken = "";
   validAccessToken = false;
   idToken = "";
+  jsonIdToken = "";
   validIdToken = false;
   players: Player[];
   filteredList: Player[];
@@ -99,6 +100,7 @@ export class AppComponent  {
     this.validIdToken = this.oauthService.hasValidIdToken();
     this.accessToken = this.oauthService.getAccessToken();
     this.idToken = this.oauthService.getIdToken();
+    this.jsonIdToken = this.decodeToken( this.idToken );
 
     try {
       let self = this;
@@ -115,6 +117,31 @@ export class AppComponent  {
     } finally {
       this.filteredList = this.players as Player[];
     }
+  }
+
+    private urlBase64Decode(str: string) {
+          var output = str.replace(/-/g, '+').replace(/_/g, '/');
+          switch (output.length % 4) {
+              case 0: { break; }
+              case 2: { output += '=='; break; }
+              case 3: { output += '='; break; }
+            default: {
+                throw 'Illegal base64url string!';
+              }
+          }
+          return decodeURIComponent(escape(window.atob(output)));
+  }
+     
+  public decodeToken(token: string) {
+      var parts = token.split('.');
+      if (parts.length !== 3) {
+          throw new Error('JWT must have 3 parts');
+    }
+      var decoded = this.urlBase64Decode(parts[1]);
+      if (!decoded) {
+          throw new Error('Cannot decode the token');
+      }
+      return JSON.parse(decoded);
   }
 
 }
